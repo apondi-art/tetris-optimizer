512ca350f88960571330f03db3bcd90f08036313
quochieng <aiyanarich64@gmail.com>
2024-11-06 21:24:51 +0300

    feat:add function for can place

--- tetris/place.go
+++ tetris/place.go
@@ -1,6 +1,42 @@
 package tetris
 
 
-// func CanPlace(board[][]string , tetriminoes []string ,x,y int)bool{
+func Place(board [][]string, tetrimino []string, x, y int) {
+	for dy := range tetrimino {
+		for dx, char := range tetrimino[dy] {
+			board[dy+y][dx+x] = string(char)
 
-// }
+		}
+	}
+
+}
+
+func CanPlace(board [][]string, tetrimino []string, x, y int) bool {
+    for dy := range tetrimino {
+        for dx := range tetrimino[dy] {
+            // Calculate the position on the board
+            boardY, boardX := dy+y, dx+x
+
+            // Check if the position is outside the bounds of the board
+            if boardY >= len(board) || boardX >= len(board[0]) || boardY < 0 || boardX < 0 {
+                return false
+            }
+
+            // Check if the board position is already occupied
+            if board[boardY][boardX] != "." {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+
+func RemoveTetris(board [][]string, tetrimino []string, x,y int){
+	for dy := range tetrimino{
+		for dx :=range tetrimino[dy]{
+			board[dy][dx] = "."
+		}
+	}
+}
+
